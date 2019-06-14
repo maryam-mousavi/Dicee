4b6c749505329d4a529c14404dbc9b1398c9c7b5
Maryam Mousavi <maryam.mousavi20@gmail.com>
2019-06-14 10:23:35 +0100

    Add missing SceneDelegate

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+
+        let diceArray = [ UIImage(named: "DiceOne"),
+                          UIImage(named: "DiceTwo"),
+                          UIImage(named: "DiceThree"),
+                          UIImage(named: "DiceFour"),
+                          UIImage(named: "DiceFive"),
+                          UIImage(named: "DiceSix")]
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+
     }
-
-
 }
 
